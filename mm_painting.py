a9caa85f895f07c5e188169438c0cd006dfc45e4
PumpkinJui <ZXN070320@163.com>
2026-09-15 17:10:21 +0800

    feat(painting): 原画文件名

--- mm_painting.py
+++ mm_painting.py
@@ -133,7 +133,11 @@ class Painting:
             lang_dict = {entry['id'].lower(): entry['name'] for entry in lang_json}
         for painting in Path(self.path_map['input']['painting']).glob('*.png'):
             stem = painting.stem
-            _ = copy2(painting, self.path_map['output_dir']['painting'] / painting.name)
+            _ = copy2(
+                painting,
+                self.path_map['output_dir']['painting']
+                / f'{painting.stem}_replacer.png',
+            )
             self.enlang.append(f'item.painting:{stem}.name={lang_dict.get(stem, stem)}')
             if stem not in lang_dict:
                 logger.warning('无译名。', extra={'pos': f'{self.POS} - {stem}'})
